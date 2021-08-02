b5dff0b15a4226fe2f67d0119ab6579280fc0cd3
Bakiev Damir <xray3d@ya.ru>
2021-08-02 20:53:30 +0300

    Создал проект

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace url_downloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
